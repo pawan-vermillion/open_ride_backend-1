49b0849f032e06b6d48aa1fa3da72cca1a924b3b
dhruv0124 <dhruv@vermilliontech.in>
2024-10-18 10:26:29 +0530

    for Pull

--- modules/admin/controller/shared/userController.js
+++ modules/admin/controller/shared/userController.js
@@ -2,17 +2,22 @@ const UserService = require("../../services/shared/userService");
 
 
 class UserController {
+
+
   getUser = async (req, res, next) => {
     try {
       
       const UserId = req.user.id;
-      const { limit, page } = req.query;
-      const result = await UserService.getUser({ UserId  ,limit , page });
+      const { limit, page , search} = req.query;
+      const result = await UserService.getUser({ UserId  ,limit , page,search });
       return res.status(200).json(result);
     } catch (error) {
       res.status(404).json({ message: error.message })
     }
   }
+
+
+
   getUserById = async (req, res, next) => {
     try {
       const  userId  = req.params.id;  
--- modules/admin/services/shared/userService.js
+++ modules/admin/services/shared/userService.js
@@ -2,15 +2,31 @@ const User = require("../../../user/model/user")
 
 class UserService {
 
-
-    async getUser({limit, page}) {
+    async getUser({ limit, page, search }) {
         try {
             const pageSize = parseInt(limit) || 10;
             const currentPage = parseInt(page) || 1;
             const skip = (currentPage - 1) * pageSize;
-            const users = await User.find().select("-__v -password -updatedAt").skip(skip)
+
+            // Search condition add kari che
+            const searchCondition = search
+                ? {
+                    $or: [
+                        { name: { $regex: search, $options: "i" } },  // i means case-insensitive
+                        { email: { $regex: search, $options: "i" } },
+                        { phone: { $regex: search, $options: "i" } }
+                    ]
+                }
+                : {};
+
+            // Users ni query ma search condition add kari
+            const users = await User.find(searchCondition)
+                .select("-__v -password -updatedAt")
+                .skip(skip)
                 .limit(pageSize);
-                const total = await User.countDocuments()
+
+            const total = await User.countDocuments(searchCondition); // total ne pan search pramane count kariyu
+
             if (!users) {
                 const error = new Error("User not found");
                 error.statusCode = 404;
@@ -20,16 +36,19 @@ class UserService {
             return {
                 page: currentPage,
                 limit: pageSize,
-                totalUser:total,
+                totalUser: total,
                 users
             };
 
         } catch (error) {
             throw error;
         }
-
     }
-    async getUserById( userId ) {
+
+
+
+
+    async getUserById(userId) {
         try {
             const user = await User.findById(userId).select("-__v -password -updatedAt");
 
