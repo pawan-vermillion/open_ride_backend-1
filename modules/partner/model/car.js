68ec1cbb5a1187d565c99245f6ed2eea2a9f5508
Pooja-Kacha <pooja@vermilliontech.in>
2025-01-01 12:39:13 +0530

    add hybrid

--- modules/partner/model/car.js
+++ modules/partner/model/car.js
@@ -20,7 +20,7 @@ const CarDetailsSchema = new mongoose.Schema({
     },
     fuelType:{
         type:String,
-        enum:["Diesel" , "Petrol" , "CNG" , "Electric"],
+        enum:["Diesel" , "Petrol" , "CNG" , "Electric" , "Hybrid" ],
         required:true
     },
     transmission:{
